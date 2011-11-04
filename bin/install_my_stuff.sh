04d69f488a68f92491f139681e57be84d23c114c
David Stolarsky <david.stolarsky@gmail.com>
2011-11-04 14:03:36 -0400

    fixes, additions

--- bin/install_my_stuff.sh
+++ bin/install_my_stuff.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
+apt-get install git
+apt-get install subversion
+
+apt-get install zsh
+
+apt-get install vim-gnome
+
 # liblo (OSC) and oscsend (oscdump and oscsend)
-sudo apt-get install liblo-dev
+apt-get install liblo-dev
 wget -O /tmp/oscsend.tar.gz http://fukuchi.org/works/oscsend/oscsend-1.0.0.tar.gz
 cd /tmp
 tar xzvf oscsend.tar.gz
+cd oscsend-1.0.0
 ./configure
 make
 sudo make install
